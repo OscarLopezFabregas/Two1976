379240517aabc187e46e13f709bd5141c0eaa8c7
OscarLopezFabregas <oscar.lopez6@alumni.esade.edu>
2018-03-27 00:05:43 +0200

    Added Movement to the Bat

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    Rigidbody rb;
+    public float initialSpeed = 600f;
+    bool inGame = false;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+         
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(!inGame && Input.GetButtonDown("Fire1"))
+        {
+            inGame = true;
+            transform.SetParent(null);
+
+        }
+	}
+}
--- Assets/Scripts/BatController.cs
+++ Assets/Scripts/BatController.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BatController : MonoBehaviour {
 
+    public float speed = 0.4f;
+   
+    Vector3 initPos;
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        initPos = transform.position;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+    public void Reset()
+    {
+        transform.position = initPos;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        float horizontalKeyboard = Input.GetAxisRaw("Horizontal");
+        float posX = transform.position.x + horizontalKeyboard * speed * Time.deltaTime;
+
+        transform.position = new Vector3(Mathf.Clamp(posX, -2.175f, 2.175f), transform.position.y, transform.position.z);
 	}
 }
