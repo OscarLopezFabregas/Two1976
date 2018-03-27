35f7eb958a201e958be31dd645bb4bf3bb4022d1
OscarLopezFabregas <32035406+OscarLopezFabregas@users.noreply.github.com>
2018-03-27 17:44:57 +0200

    added brick breaking with particles

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+
+    public GameObject particles;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(particles, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+    //TODO: POWER UP, WHEN POWER UP ON, ISTRIGGER = TRUE, AND USE ONTRIGGERENTER;
+   
+}
