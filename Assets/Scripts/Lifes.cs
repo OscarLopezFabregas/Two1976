a9d9dab97c818a51d6d21b0b8c2ed6b10e4e5128
OscarLopezFabregas <32035406+OscarLopezFabregas@users.noreply.github.com>
2018-03-28 09:53:29 +0200

    Added GameManager, Added Life System

---
+++ Assets/Scripts/Lifes.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lifes : MonoBehaviour {
+
+    //static forces int lifes to exist only ONCE in the whole app.
+    //Global
+    //We will be able to access from everywhere in the program
+    public static int lifes = 3;
+    public Text lifeText;
+    public BallController Ball;
+    public BatController Bat;
+
+    private void Start()
+    {
+        UpdateLifes();
+    }
+
+    void UpdateLifes()
+    {
+        lifeText.text = "Lifes: " + Lifes.lifes;
+
+    }
+    public void MinusOneLife()
+    {
+        Lifes.lifes--;
+        UpdateLifes();
+        Bat.Reset();
+        Ball.Reset();
+    }
+    private void Update()
+    {
+       
+    }
+}
