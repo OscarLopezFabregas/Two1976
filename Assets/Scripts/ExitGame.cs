56c90687fd5c9cae7601c0c2def61ae60d9dddd6
OscarLopezFabregas <32035406+OscarLopezFabregas@users.noreply.github.com>
2018-03-28 15:46:14 +0200

    added start game function

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitGame : MonoBehaviour {
+
+    public bool exit;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (exit)
+            {
+                Application.Quit();
+            }
+            else
+            {
+                SceneManager.LoadScene("Portrait");
+            }
+        }
+	}
+}
