e5b58c2f1b287585bf568817e05ae8d1d5fed5ee
OscarLopezFabregas <32035406+OscarLopezFabregas@users.noreply.github.com>
2018-04-09 15:55:05 +0200

    Set Options Modal Panel

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+    public GameObject options;
+
+	public void Activate(GameObject panel)
+    {
+        panel.transform.SetAsLastSibling();
+        panel.SetActive(true);
+    }
+
+    public void Deactivate(GameObject panel)
+    {
+        panel.SetActive(false);
+    }
+}
