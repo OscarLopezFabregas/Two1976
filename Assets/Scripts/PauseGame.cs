14e27faf3474137e20791e5987325524c25638a5
OscarLopezFabregas <32035406+OscarLopezFabregas@users.noreply.github.com>
2018-04-09 18:12:41 +0200

    Keep changing pause menus

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+
+    public static bool gameIsPaused = false;
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+    }
+
+    
+}
