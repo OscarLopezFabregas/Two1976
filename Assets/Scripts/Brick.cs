e86ff2cc27e6901ccd89a6670073acf62144d10d
OscarLopezFabregas <32035406+OscarLopezFabregas@users.noreply.github.com>
2018-04-12 15:58:30 +0200

    Now on hit, bricks with multiple hit points change color!

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -6,12 +6,25 @@ public class Brick : MonoBehaviour {
 
     public GameObject particles;
     public Points points;
-
+    public int hits = 1;
+    Material mat;
+    private void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
     private void OnCollisionEnter(Collision collision)
     {
-        Instantiate(particles, transform.position, Quaternion.identity);
-        Destroy(gameObject);
-        transform.SetParent(null);
+        hits--;
+    
+
+        if(hits<=0)
+        {
+            Instantiate(particles, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+            transform.SetParent(null);
+        }
+
+        mat.color = Color.yellow;
         points.WinPoints();
     }
     //TODO: POWER UP, WHEN POWER UP ON, ISTRIGGER = TRUE, AND USE ONTRIGGERENTER;
